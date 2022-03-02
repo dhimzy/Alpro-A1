c6321b6f5ec6d4d243aab02c506ac795b7ccd0cc
dhimzy <100180259+dhimzy@users.noreply.github.com>
2022-03-02 10:20:11 +0700

    Add files via upload

---
+++ Praktikum2/LuasBujursangkar_DhiyaMazaya.c
@@ -0,0 +1,22 @@
+/*Nama File: LuasBujursangkar.c*/
+/*Deskripsi: Menghitung dan menampilkan luas bujursangkar*/
+/*Pembuat: Dhiya Mazaya - 24060121140151*/
+/*Tgl. Pembuatan: Rabu, 2-3-2022 9:11 WIB*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+/*Kamus*/
+    int s; //sisi bujursangkar
+    int l; //luas bujursangkar
+    s = 10;
+
+/*Algoritma*/
+    l = s*s;
+    printf("Sisi = %d\n", s);
+    printf("Luas Bujur sangkar = %d\n", l);
+
+    return 0;
+}
